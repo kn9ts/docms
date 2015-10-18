7ba4862c8571fe0143afdcc2db329601ef43a091
Eugene Mutai <eugenemutai@gmail.com>
2015-10-18 13:42:21 +0300

    deleted app folder, not required

--- app/scripts/factories/todo.resource.js
+++
@@ -1,9 +0,0 @@
-function todoResource($resource) {
-  return $resource('/api/todos/:id', null, {
-    update: {
-      method: 'PUT' // This method issues a PUT request
-    }
-  });
-}
-
-module.exports = todoResource;
