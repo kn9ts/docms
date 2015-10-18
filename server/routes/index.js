cbf7fd3c4cf2b510b6a42736b7ae050b660e9913
Eugene Mutai <eugenemutai@gmail.com>
2015-10-18 13:45:36 +0300

    refactored index route to return json file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,13 @@
 module.exports = function(app, config) {
   require('./users')(app, config);
+  // require('./documents')(app, config);
+  // require('./roles')(app, config);
 
   /* GET home page. */
   app.get('/*', function(req, res, next) {
-    res.render('index', {
-      title: 'Welcome to the document management system api'
+    res.status(200).json({
+      status: 200,
+      message: 'Welcome to the document management system api'
     });
   });
 };
